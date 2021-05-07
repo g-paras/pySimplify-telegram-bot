fa79ffca435adff674afec355046b4d4d2d522ec
Paras Gupta <guptaparas039@gmail.com>
2021-05-07 12:38:12 +0530

    testing

--- bot.py
+++ bot.py
@@ -59,10 +59,7 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://pys-telegram-bot.herokuapp.com/' + TOKEN)
+    updater.start_pooling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
