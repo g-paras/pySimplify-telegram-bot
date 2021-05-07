36af695fddc55533722040366a8abe16fb534200
Paras Gupta <guptaparas039@gmail.com>
2021-05-07 12:33:17 +0530

    changed web app url

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://pys-telegram-bot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
